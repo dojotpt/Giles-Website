5e2bb1d22deee025cbaf07985e24b1e9f5a8daf9
Don Johnson <donjohnsontpt@gmail.com>
2024-09-30 14:42:13 -0400

    added card1 to homepage. Needs flex container and formatting

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,30 +1,30 @@
 <template>
     <div id="entire-page-container">
-  <header id="app-header">
-    <div class="logo">
-    <img id="logo-img" src="@/images/headerImage.jpg" alt="picture of wedding rings">
-    </div>
-
-    <div class="nav">
-      <router-link class="nav-a" v-bind:to="{ name: 'home' }">Home</router-link>
-      <router-link class="nav-a" v-bind:to="{ name: 'about' }">About</router-link>
-      <router-link class="nav-a" v-bind:to="{ name: 'booking' }">Booking</router-link>
-      <router-link class="nav-a" v-bind:to="{ name: 'highlights' }">Highlights</router-link>
-      <router-link class="nav-a" v-bind:to="{ name: 'reviews' }">Reviews</router-link>
-      <router-link class="nav-a" v-bind:to="{ name: 'contact' }">Contact</router-link>
-      <router-link class="nav-a" v-bind:to="{ name: 'faq' }">FAQ</router-link>
-    </div>
-    <div class="s-media">
-      <img src="@/images/instagram.png" class="social-icon" alt="instagram" />
-      <img src="@/images/linkedin.png" class="social-icon" alt="linkedin" />
-      <img src="@/images/facebook.png" class="social-icon" alt="facebook" />
-      <img src="@/images/x.png" class="social-icon" alt="link to x"/>      
-    </div>
-  </header>
+       <header id="app-header">
+            <div class="logo">
+            <img id="logo-img" src="@/images/headerImage.jpg" alt="picture of wedding rings">
+            </div>
+        <div class="nav">
+            <router-link class="nav-a" v-bind:to="{ name: 'home' }">Home</router-link>
+            <router-link class="nav-a" v-bind:to="{ name: 'about' }">About</router-link>
+            <router-link class="nav-a" v-bind:to="{ name: 'booking' }">Booking</router-link>
+            <router-link class="nav-a" v-bind:to="{ name: 'highlights' }">Highlights</router-link>
+            <router-link class="nav-a" v-bind:to="{ name: 'reviews' }">Reviews</router-link>
+            <router-link class="nav-a" v-bind:to="{ name: 'contact' }">Contact</router-link>
+            <router-link class="nav-a" v-bind:to="{ name: 'faq' }">FAQ</router-link>
+          </div>
+        <div class="s-media">
+            <img src="@/images/instagram.png" class="social-icon" alt="instagram" />
+            <img src="@/images/linkedin.png" class="social-icon" alt="linkedin" />
+            <img src="@/images/facebook.png" class="social-icon" alt="facebook" />
+            <img src="@/images/x.png" class="social-icon" alt="link to x"/>      
+          </div>
+      </header>
+  
   <div id=background>
   <router-view />
-</div>
-</div>
+    </div>  
+    </div>
 </template>
 
 <style>
@@ -37,27 +37,37 @@
     linear-gradient(rgba(245, 245, 220, 0.37), rgba(245, 245, 220, 0.2)),
     url('/src/images/sample/abby-savage-zNsSwsuyP3A-unsplash.jpg'),
     url('/src/images/sample/abby-savage-zNsSwsuyP3A-unsplash.jpg');
-  background-size: cover, cover, cover;
-  background-position: center, center, center;
-  background-repeat: no-repeat, no-repeat, no-repeat;
-  background-attachment: fixed, fixed, fixed;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
   background-blend-mode: normal, multiply;
+  padding-top: 20px;
 }
+
+#entire-page-container {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  margin: 0;
+  padding: 0;
+}
+
 header {
   display: grid;
   grid-template-columns: auto 1fr auto;
   grid-template-areas:
     "logo nav s-media";
   align-items: center;
-  background-color: #36454F;
+  background-color: #F5F5DC;
   padding: .25% .25% 0 .25%;
   border-bottom: 2px solid #D4AF37;
-  height: 11vh;
+  height: 13vh;
+  
 }
-h1 {
-  margin: 0;
-  padding: 0;
-}
+
 .logo {
   grid-area: logo;
   display: flex;
@@ -67,14 +77,16 @@ h1 {
 }
 
 #logo-img {
+  grid-area: logo;
   height: 100%;
   width: 15%;
-  border-radius: 50%;
+  border-radius:  50%;
+  border: 2px solid #D4AF37;
 }
 
 .nav {
   grid-area: nav;
-  color: #F5F5DC;
+  color: #36454F;
   display: flex;
   justify-content:center;
   align-items: center;
@@ -88,7 +100,7 @@ h1 {
   padding: 5%;
 }
 
-#nav a:hover {
+.nav-a:hover {
   color: #D4AF37;
 }
 
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
+      <div class="page-container">
+    
+      <div id="card1" class="card"> 
+        <h1 id="about1">Jordan Giles</h1>
+        <h2 id="about2">Wedding Officiant</h2>
+        <img id="card1-image" src="@\images\headshot.jpeg" alt="wedding image">
+      </div>
+    </div>
+  
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+
   </div>
 </template>
 
@@ -11,4 +19,42 @@ export default {
 </script>
 
 <style scoped>
+#page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+#card1 {
+
+}
+.card {
+  display:grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas:
+  "about1 card1-image"
+  "about2 card1-image";
+  background-color:rgba(245, 245, 220, 0.6);
+  height: 40vh;
+  width: 100%;
+  margin-top: 20px;
+  border-radius:  2%;
+}
+img {
+  grid-area: card1-image;
+  height: 35vh;
+  width: auto;
+  opacity: 0.85;
+  border-radius:  10%;
+}
+#about1 {
+  grid-area: about1;
+}
+#about2 {
+  grid-area: about2;
+}
 </style>
